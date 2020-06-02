dc85533fed8ae5b526c68396225064eb1da57129
Caylem <caylemh@gmail.com>
2020-06-02 17:48:21 +0200

    CH4: Varargs notation

--- src/ch4_Statistics/StatsTest.java
+++ src/ch4_Statistics/StatsTest.java
@@ -4,7 +4,7 @@ public class StatsTest {
     public static void main(String[] args) {
         Statistics stats = new Statistics();
 
-        float average = stats.average(2,3,67,8,3,4,5);
+        float average = stats.average(2,3,67,8,3,4,5); // Using the varargs notation. Treats the 'nums' parameter as an array.
 
         System.out.println("The average of the arrays = " + average );
     }

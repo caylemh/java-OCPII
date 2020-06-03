b26bfabf8c4957a5f895ce37aa5e61a99021c713
Caylem <caylemh@gmail.com>
2020-06-03 22:32:52 +0200

    CH6: String Analyzer Exercise - Using Interface

---
+++ src/ch_StringAnalyzerExercise/InterfaceTest.java
@@ -0,0 +1,20 @@
+package ch_StringAnalyzerExercise;
+
+public class InterfaceTest {
+    public static void main(String[] args) {
+        String[] strList =
+                {"tomorrow","toto","to","timbukto","the","hello","heat"};
+        String searchStr = "to";
+        System.out.println("Searching for: " + searchStr);
+
+        // Call concrete class that implements StringAnalyzer
+        ContainsAnalyzer contains = new ContainsAnalyzer();
+
+        System.out.println("===Contains===");
+        for(String currentStr:strList){
+            if (contains.analyze(currentStr, searchStr)){
+                System.out.println("Match: " + currentStr);
+            }
+        }
+    }
+}

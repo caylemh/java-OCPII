f88eb79d19addf4329d0710d2b1df6a1ff62d15f
Caylem <caylemh@gmail.com>
2020-06-03 07:10:46 +0200

    CH4: Immutable Classes

---
+++ src/ch4_StaticVarMethod/ContactsTest.java
@@ -0,0 +1,9 @@
+package ch4_StaticVarMethod;
+
+public class ContactsTest {
+    public static void main(String[] args) {
+
+        Contacts c1 = new Contacts("Sarah", "Holden","012-345-6789");
+        System.out.println(c1);
+    }
+}

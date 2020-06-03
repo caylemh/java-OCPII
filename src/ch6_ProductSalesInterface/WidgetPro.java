5ba0da30324f4515cc28dffc84c3d0888ee8af86
Caylem <caylemh@gmail.com>
2020-06-03 21:24:23 +0200

    CH6: Implementing & Extending

---
+++ src/ch6_ProductSalesInterface/WidgetPro.java
@@ -0,0 +1,15 @@
+package ch6_ProductSalesInterface;
+
+public class WidgetPro extends Widget implements WidgetSalesCalcs{
+    private String type;
+
+    public WidgetPro(double salesPrice, double cost, long quantity, String type){
+        super(salesPrice, cost, quantity);
+        this.type = type;
+    }
+
+    public String getWidgetType(){
+        return type;
+    }
+
+}

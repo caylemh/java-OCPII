d9e0560067173fc35577e54c1be9277491573e0e
Caylem <caylemh@gmail.com>
2020-06-05 11:07:13 +0200

    CH5: Examples

---
+++ src/ch5_AbstractElectronicDevice/ElectronicDevice.java
@@ -0,0 +1,8 @@
+package ch5_AbstractElectronicDevice;
+
+public abstract class ElectronicDevice {
+
+    public abstract void turnOn();
+
+    public abstract void turnOff();
+}
---
+++ src/ch5_AbstractElectronicDevice/Television.java
@@ -0,0 +1,14 @@
+package ch5_AbstractElectronicDevice;
+
+public class Television extends ElectronicDevice {
+
+    public void turnOn() {
+        changeChannel(1);
+        initializeScreen();
+    }
+    public void turnOff() {}
+
+    public void changeChannel(int channel) {}
+    public void initializeScreen() {}
+
+}

b35bab3ccc250b91d42bbc2d73b31caaf73d565a
Caylem <caylemh@gmail.com>
2020-06-07 17:40:44 +0200

    CH7: Generics & Collections

---
+++ src/ch7_GenericsCollections/generics/CacheString.java
@@ -0,0 +1,17 @@
+
+package ch7_GenericsCollections.generics;
+
+
+public class CacheString {
+  
+    private String message;
+  
+    public void add(String message){
+        this.message = message;
+    }
+  
+    public String get(){
+        return this.message;
+    }  
+  
+}
